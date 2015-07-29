8a96ce02fcbde9d6d251c9933c73f2d3c7b22e58
Artifacte <rajat.kulshreshtah50@gmail.com>
2015-07-29 18:44:47 +0530

    removed errors with variable parsing and delimiters

--- Chess.java
+++ Chess.java
@@ -8,16 +8,19 @@ public class Chess {
     public static void main(String[] args) throws java.lang.Exception {
         try (Scanner reader = new Scanner(System.in)) {
             System.out.println("Enter the file name (e.g. abc.txt)");
-            String file = reader.nextLine();
+            //String file = reader.nextLine();
+            ChessBoard chessBoard = new ChessBoard();
+            String file = "chessmoves.txt";
             BufferedReader br = new BufferedReader(new FileReader(".\\src\\chess\\" + file));
             String line = br.readLine();
             System.out.println(line);
-            String[] moves = line.replaceAll("\\.", " ").split("\\s+");
-            for (int i=0; i<moves.length ;i++){
-                if (i%3 != 0 && moves[i].matches(".*[a-zA-Z]+.*")) {
-                    System.out.print("  " + moves[i]);
+            String[] moves = line.replaceAll(" *\\d*[.] *" ," ").split(" ");
+            System.out.println(Arrays.toString(moves));
+            for (int i=1; i<moves.length ;i+= 2){
+                if ( moves[i].matches(".*[a-zA-Z]+.*")) {
+                    chessBoard.update(moves[i], moves[i+1]);
                 }
-            }            
+            }
         }
         
     }
