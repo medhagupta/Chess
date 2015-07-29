8d644b7508aeaa31ef0f36a5f78f8b5e1ff92e42
Artifacte <rajat.kulshreshtah50@gmail.com>
2015-07-29 13:37:22 +0530

    added user input to file name

--- Chess.java
+++ Chess.java
@@ -1,15 +1,16 @@
 package chess;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.*;
 
 public class Chess {
     public static void main(String[] args) throws java.lang.Exception {
         Scanner reader = new Scanner(System.in);
+        System.out.println("Enter the file name (e.g. abc.txt)");
         String file = reader.nextLine();
         BufferedReader br = new BufferedReader(new FileReader(file));
         String line = br.readLine();
+        System.out.println(line);
     }
 }
