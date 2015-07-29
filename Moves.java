fa5cb1a0377ae8c31e1db0266540415d688c0050
unknown <test@L-MAA-25004062.paypalcorp.com>
2015-07-29 16:05:14 +0530

    Updated function names

--- Moves.java
+++ Moves.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -9,7 +10,7 @@ public class Moves {
 
 
 
-    public static boolean isvalidQueen(String start,String end,Map<String,String> ChessBoard)
+    public static boolean isvalidQueenMove(String start,String end,Map<String,String> ChessBoard)
     {
 
 
@@ -17,12 +18,12 @@ public class Moves {
     }
 
 
-    public static boolean isvalidKing(String s1,String s2,Map<String,String> ChessBoard)
+    public static boolean isvalidKingMove(String s1,String s2,Map<String,String> ChessBoard)
     {
         return true;
     }
 
-    public static boolean isvalidKight(String start,String end,Map<String,String> ChessBoard)
+    public static boolean isvalidKightMove(String start,String end,Map<String,String> ChessBoard)
     {
 
 
@@ -39,7 +40,7 @@ public class Moves {
         return true;
     }
 
-    public static boolean isvalidBishop(String start,String end, Map<String,String> ChessBoard){
+    public static boolean isvalidBishopMove(String start,String end, Map<String,String> ChessBoard){
 
         // Camel
 
@@ -74,7 +75,7 @@ public class Moves {
 
     }
 
-    public static boolean isvalidRook(String start,String end,Map<String,String> ChessBoard){
+    public static boolean isvalidRookMove(String start,String end,Map<String,String> ChessBoard){
 
         // Elephant
 
@@ -107,10 +108,9 @@ public class Moves {
             String Position;
             for (int i = minimum + 1; i < maximum; i++) {
                 Position = start.charAt(0) + Integer.toString(i);
-                if (ChessBoard.containsValue(Position)) {
+                if (ispawnpresent(Position,ChessBoard)) {
                     return false;
                 }
-
             }
         }
         return true;
@@ -146,11 +146,57 @@ public class Moves {
 
     public static boolean ispawnpresent(String Position, Map<String,String> ChessBoard)
     {
+        /*
         if ( ChessBoard.containsValue(Position))
         {
             return true;
         }
         return false;
+        */
+        return true;
     }
+
+    public static void main(String args[]) {
+        Map<String,String> chessBoard = new HashMap<String,String>();
+
+
+            chessBoard.put("Rw1","a1");
+            chessBoard.put("Nw1","b1");
+            chessBoard.put("Bw1","c1");
+            chessBoard.put("Qw","d1");
+            chessBoard.put("Kw","e1");
+            chessBoard.put("Bw2","f1");
+            chessBoard.put("Nw2","g1");
+            chessBoard.put("Rw2","h1");
+            chessBoard.put("Pw1","a2");
+            chessBoard.put("Pw2","b2");
+            chessBoard.put("Pw3","c2");
+            chessBoard.put("Pw4","d2");
+            chessBoard.put("Pw5","e2");
+            chessBoard.put("Pw6","f2");
+            chessBoard.put("Pw7","g2");
+            chessBoard.put("Pw8","h2");
+
+            chessBoard.put("Rb1","a8");
+            chessBoard.put("Nb1","b8");
+            chessBoard.put("Bb1","c8");
+            chessBoard.put("Qb","d8");
+            chessBoard.put("Kb","e8");
+            chessBoard.put("Bb2","f8");
+            chessBoard.put("Nb2","g8");
+            chessBoard.put("Rb2","h8");
+            chessBoard.put("Pb1","a7");
+            chessBoard.put("Pb2","b7");
+            chessBoard.put("Pb3","c7");
+            chessBoard.put("Pb4","d7");
+            chessBoard.put("Pb5","e7");
+            chessBoard.put("Pb6","f7");
+            chessBoard.put("Pb7","g7");
+            chessBoard.put("Pb8","h7");
+
+        System.out.print(isValidPawnMove("b2","b3",chessBoard));
+
+
+}
 
 }
